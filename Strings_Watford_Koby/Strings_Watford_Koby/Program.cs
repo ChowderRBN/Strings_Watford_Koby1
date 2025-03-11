8651e9c2e76118d89c33175542c1ec65f2f8fa8f
Koby Watford <kobywatford8@icloud.com>
2025-03-11 12:44:42 -0500

    Add files via upload

---
+++ Strings_Watford_Koby/Strings_Watford_Koby/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Ask for a character input
+        Console.Write("Please input a character!\n");
+        char myChar = Console.ReadKey().KeyChar;
+        bool isLetter = Char.IsLetter(myChar);
+        Console.WriteLine($"\n{myChar} is a letter: {isLetter}\n");
+
+        // Ask for a string input
+        Console.WriteLine("Why do you like video games?");
+        string string1 = Console.ReadLine();
+
+        // Ask for a word to search
+        Console.WriteLine("Enter a word");
+        string string2 = Console.ReadLine();
+
+        // Check if the word exists in the string
+        bool containsWord = string1.Contains(string2);
+        Console.WriteLine($"{string2} exists in {string1}, {containsWord}!");
+    }
+}
